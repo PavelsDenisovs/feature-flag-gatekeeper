daa39fc6bcebeee2c41f68d7904a2f7caa0504a8
Pavel Denisov <pavelsdenisovs1@gmail.com>
2026-02-20 11:52:37 +0200

    chore: add main entrypoint and CLI stub
    Add main.go as process entrypoint and cli package with
    a stub method Run.

---
+++ cmd/ffg/cli/cli.go
@@ -0,0 +1,6 @@
+package cli
+
+// Run executes CLI and returns process exit code. It expects arguments without program name
+func Run(args []string) (exitCode int) {
+	return 0
+}
--- cmd/ffg/main.go
+++ cmd/ffg/main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"os"
+	"github.com/PavelsDenisovs/feature-flag-gatekeeper/cmd/ffg/cli"
+)
+
+func main() {
+	os.Exit(cli.Run(os.Args[1:]))
+}
