00ed577bc00f49844b8c2750f7c71e10751bc96a
Pavel Denisov <pavelsdenisovs1@gmail.com>
2026-02-24 17:05:41 +0200

    fix(server/http): add missing config fields and rename pkg
    - rename the package from trasnport to server
    - add Port and DBUrl missing config fields

--- internal/server/http/http.go
+++ internal/server/http/http.go
@@ -1,11 +1,14 @@
-package transport
+package http
 
 import (
 	"database/sql"
 	"net/http"
 )
 
-type HTTPConfig struct {}
+type HTTPConfig struct {
+	Port int
+	DBUrl string
+}
 
 func NewHTTPServer(cfg HTTPConfig, mux *http.ServeMux, db *sql.DB) *http.Server {
 	return nil
